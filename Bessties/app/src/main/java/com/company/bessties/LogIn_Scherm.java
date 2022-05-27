af758d7f63d216a53ce47e47a4b46da764b3a03a
Bram Laros <bramlaros@gmail.com>
2022-05-27 15:16:08 +0200

    Added login layout

--- Bessties/app/src/main/java/com/company/bessties/LogIn_Scherm.java
+++ Bessties/app/src/main/java/com/company/bessties/LogIn_Scherm.java
@@ -3,8 +3,13 @@ package com.company.bessties;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LogIn_Scherm extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
