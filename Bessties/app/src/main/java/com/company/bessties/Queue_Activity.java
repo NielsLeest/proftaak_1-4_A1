1a7e1677272be445e195980c22ef8a72673641ac
Tommy <tdc.vanlangevelde@sudent.avans.nl>
2022-05-31 15:06:00 +0200

    Lay-Out Queue

--- Bessties/app/src/main/java/com/company/bessties/Queue_Activity.java
+++ Bessties/app/src/main/java/com/company/bessties/Queue_Activity.java
@@ -6,15 +6,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class ProfileView_Activity extends AppCompatActivity {
+public class Queue_Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile_view_screen);
+        setContentView(R.layout.activity_queue_screen);
     }
 
-    public void openQueue(View view) {
+    public void openBuddyLocation(View view) {
+        Intent intent = new Intent(this, GameInfo_Activity.class);
+        startActivity(intent);
+    }
+
+    public void retryQueue(View view) {
         Intent intent = new Intent(this, Queue_Activity.class);
         startActivity(intent);
     }
