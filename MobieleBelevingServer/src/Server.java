e0564e70f792e1603e8536251f4332b7ad757a28
midas <midzzflitzz@gmail.com>
2022-06-09 12:22:41 +0200

    added protocol

--- MobieleBelevingServer/src/Server.java
+++ MobieleBelevingServer/src/Server.java
@@ -38,6 +38,9 @@ public class Server {
                 case "kill":
                     killserver();
                     break;
+                case "clients":
+                    fo;
+                    break;
             }
         }
     }
