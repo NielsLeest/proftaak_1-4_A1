a06b51105b0a0426f1e45ede2a076134c96bdd39
Kevin <kevinjlhu@hotmail.nl>
2022-06-13 15:53:46 +0200

    Added profile picture selector in Account creation
    Able to choose a profile picture in Account creation
    Able to send the selected picture to Profile Overview

--- Bessties/app/src/main/java/com/company/bessties/LogIn_Activity.java
+++ Bessties/app/src/main/java/com/company/bessties/LogIn_Activity.java
@@ -1,15 +1,20 @@
 package com.company.bessties;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.company.bessties.socket.Client;
+
+import java.io.ByteArrayOutputStream;
 
 public class LogIn_Activity extends AppCompatActivity {
     String firstname;
@@ -24,10 +29,16 @@ public class LogIn_Activity extends AppCompatActivity {
     Client client;
     boolean isValid = false;
 
+    private int currentImage = 0;
+    int[] images = {R.drawable.avatar_icon, R.drawable.appicon, R.drawable.qr_scanner};
+    ImageView currentImageView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in_screen);
+
+        currentImageView = (ImageView) findViewById(R.id.profilePicture);
 
         this.client = new Client();
         new Thread(this.client::startConnection).start();
@@ -70,7 +81,30 @@ public class LogIn_Activity extends AppCompatActivity {
     }
 
     public void volgend(View view) {
+        Bitmap bmp = BitmapFactory.decodeResource(getResources(), images[currentImage]);
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        bmp.compress(Bitmap.CompressFormat.PNG, 100, stream);
+        byte[] byteArray = stream.toByteArray();
+
         Intent intent = new Intent(this, ProfileView_Activity.class);
+        intent.putExtra("picture", byteArray);
         startActivity(intent);
     }
+
+    public void leftArrowButton(View view) {
+        currentImage--;
+        if (currentImage < 0){
+            currentImage = images.length - 1;
+        }
+        currentImageView.setImageResource(images[currentImage]);
+    }
+
+    public void rightArrowButton(View view) {
+        currentImage++;
+        if (currentImage >= images.length){
+            currentImage = 0;
+        }
+        currentImageView.setImageResource(images[currentImage]);
+    }
+
 }
--- Bessties/app/src/main/java/com/company/bessties/ProfileView_Activity.java
+++ Bessties/app/src/main/java/com/company/bessties/ProfileView_Activity.java
@@ -3,6 +3,9 @@ package com.company.bessties;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,37 +14,22 @@ import android.widget.ImageView;
 
 public class ProfileView_Activity extends AppCompatActivity {
 
-    private int currentImage = 0;
-    int[] images = {R.drawable.avatar_icon, R.drawable.appicon, R.drawable.qr_scanner};
-    ImageView currentImageView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_view_screen);
 
-        currentImageView = (ImageView) findViewById(R.id.profilePicture);
+        Bundle extras = getIntent().getExtras();
+        byte[] byteArray = extras.getByteArray("picture");
 
+        Bitmap bmp = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
+        ImageView image = (ImageView) findViewById(R.id.profileViewPicture);
+
+        image.setImageBitmap(bmp);
     }
 
     public void openQueue(View view) {
         Intent intent = new Intent(this, Queue_Activity.class);
         startActivity(intent);
-    }
-
-    public void leftArrowButton(View view) {
-        currentImage--;
-        if (currentImage < 0){
-            currentImage = images.length - 1;
-        }
-        currentImageView.setImageResource(images[currentImage]);
-    }
-
-    public void rightArrowButton(View view) {
-        currentImage++;
-        if (currentImage >= images.length){
-            currentImage = 0;
-        }
-        currentImageView.setImageResource(images[currentImage]);
     }
 }
